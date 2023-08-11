4ef34d45e2fe1b39706e11ae2d24c691249dd382
Falcon <jfalcon@D01DEVJfalcon01.local>
2023-08-11 13:30:51 -0500

    initial commit, adding all files

---
+++ index.js
@@ -0,0 +1,311 @@
+import axios from "axios";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// setting times in daylight savings time
+// to get standard time -100 from values
+let imageObj = {
+  // night
+  newsNine: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+News+at+9+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "150",
+    endTime: "230",
+  },
+  // night
+  newsTen: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+News+at+10+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "250",
+    endTime: "330",
+  },
+  // morning
+  firstNewsFive: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+First+News+at+5+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "1050",
+    endTime: "1130",
+  },
+  // morning
+  firstNewsSix: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+First+News+at+6+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "1150",
+    endTime: "1230",
+  },
+  // morning
+  firstNewsSaturday: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+First+News+Saturday+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "1250",
+    endTime: "1330",
+  },
+  // morning
+  firstNewsSunday: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+First+News+Sunday+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "1250",
+    endTime: "1330",
+  },
+  // morning
+  hotMic: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/Hot+Mic+-+1920+x+1080+-+Web+Thumbnail.jpg",
+    startTime: "1350",
+    endTime: "1430",
+  },
+  // morning
+  newsEleven: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+News+at+11+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "1550",
+    endTime: "1630",
+  },
+  // evening
+  newsFour: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+News+at+4+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "2050",
+    endTime: "2130",
+  },
+  // evening
+  newsFive: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+News+at+5+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "2150",
+    endTime: "2230",
+  },
+  // evening
+  newsFiveThirty: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+News+at+530+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "2220",
+    endTime: "2300",
+  },
+  // evening
+  newsSix: {
+    imageUrl:
+      "https://static.forumcomm.com/images/wdayplus-thumbnails/WDAY+News+at+6+-+Web+Thumbnail+-+Landscape+-+1920+x+1080.jpg",
+    startTime: "2250",
+    endTime: "2330",
+  },
+};
+
+const getLiveChannels = async (site_id, channel_id) => {
+  const response = await axios({
+    method: "GET",
+    url: `https://api.jwplayer.com/v2/sites/${site_id}/channels/${channel_id}/`,
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.JWPLAYER_SECRET,
+    },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      if (error.response) {
+        return {
+          error: {
+            status: error.response.status,
+            data: error.response.data,
+          },
+        };
+      } else {
+        return "Error", error.message;
+      }
+    });
+  return response;
+};
+
+const createThumbnail = async (site_id, media_id, imageUrl) => {
+  const response = await axios({
+    method: "POST",
+    url: `https://api.jwplayer.com/v2/sites/${site_id}/thumbnails/`,
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      Authorization: process.env.JWPLAYER_SECRET,
+    },
+    data: {
+      relationships: { media: [{ id: media_id }] },
+      upload: {
+        method: "fetch",
+        thumbnail_type: "static",
+        source_type: "custom_upload",
+        download_url: imageUrl,
+      },
+    },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      if (error.response) {
+        return {
+          error: {
+            status: error.response.status,
+            data: error.response.data,
+          },
+        };
+      } else {
+        return "Error", error.message;
+      }
+    });
+  return response;
+};
+
+const enableThumbnail = async (site_id, thumbnail_id) => {
+  const response = axios({
+    method: "patch",
+    url: `https://api.jwplayer.com/v2/sites/${site_id}/thumbnails/${thumbnail_id}/`,
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      Authorization: process.env.JWPLAYER_SECRET,
+    },
+    data: { relationships: { media: [{ is_poster: true }] } },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      if (error.response) {
+        return {
+          error: {
+            status: error.response.status,
+            data: error.response.data,
+          },
+        };
+      } else {
+        return "Error", error.message;
+      }
+    });
+  return response;
+};
+
+const isDST = (d) => {
+  let jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
+  let jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
+  return Math.max(jan, jul) !== d.getTimezoneOffset();
+};
+
+const dateCalc = () => {
+  // sets as GMT in lambda
+  const currentDate = new Date();
+  let hours = currentDate.getUTCHours();
+  let minutes = currentDate.getUTCMinutes();
+  if (minutes < 10) {
+    minutes = minutes.toString().padStart(2, 0);
+  }
+  return `${hours}${minutes}`;
+};
+
+const isDateInRange = (time, startTime, endTime) => {
+  return time > startTime && time < endTime;
+};
+
+const getImage = (imageArr) => {
+  let time = dateCalc();
+  let daylightOffset = 0;
+  if (!isDST) {
+    daylightOffset = 100;
+  }
+  let imageUrl = "";
+
+  const image = Object.keys(imageArr).forEach((event) => {
+    if (
+      time > imageArr[event].startTime - daylightOffset &&
+      time < imageArr[event].endTime - daylightOffset
+    ) {
+      imageUrl = imageArr[event].imageUrl;
+    }
+  });
+  return imageUrl;
+};
+
+let event = {
+  event: "channel_active",
+  channel_id: "oQI9YDnI",
+  webhook_id: "JsV8pNyB",
+  site_id: "l0XScfRd",
+  event_time: "2023-08-11T16:00:23+00:00",
+};
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const handler = async (event) => {
+  if (event === undefined) {
+    return {
+      status: 401,
+      message: "malformed event",
+    };
+  }
+  if (event.channel_id !== "oQI9YDnI") {
+    return;
+  }
+  const channel = await getLiveChannels(event.site_id, event.channel_id);
+  if (channel.status !== "active") {
+    return { message: "no active wday stream" };
+  }
+  // need to check for media_id and status = active
+  const liveEvent = channel.recent_events[0];
+  if (liveEvent.status !== "active") {
+    return { message: "no active wday event" };
+  }
+  // calculate date and get corresponding event
+  const image = getImage(imageObj);
+  const thumbnail = await createThumbnail(
+    event.site_id,
+    liveEvent.media_id,
+    image
+  );
+  if (thumbnail.status === "processing") {
+    await sleep(10000);
+    const response = await enableThumbnail(event.site_id, thumbnail.id);
+    return response;
+  } else {
+    return await enableThumbnail(event.site_id, thumbnail.id);
+  }
+};
+
+// Used to test locally
+// const main = async (event) => {
+//   if (event === undefined) {
+//     return {
+//       status: 401,
+//       message: "malformed event",
+//     };
+//   }
+//   if (event.channel_id !== "oQI9YDnI") {
+//     return;
+//   }
+//   const channel = await getLiveChannels(event.site_id, event.channel_id);
+//   if (channel.status !== "active") {
+//     return { message: "no active wday stream" };
+//   }
+//   // need to check for media_id and status = active
+//   const liveEvent = channel.recent_events[0];
+//   if (liveEvent.status !== "active") {
+//     return { message: "no active wday event" };
+//   }
+//   // calculate date and get corresponding event
+//   const image = getImage(imageObj);
+//   const thumbnail = await createThumbnail(
+//     event.site_id,
+//     liveEvent.media_id,
+//     image
+//   );
+//   if (thumbnail.status === "processing") {
+//     await sleep(10000);
+//     const response = await enableThumbnail(event.site_id, thumbnail.id);
+//     return response;
+//   } else {
+//     return await enableThumbnail(event.site_id, thumbnail.id);
+//   }
+// };
+
+// main(event);
---
+++ test.js
@@ -0,0 +1,71 @@
+let ob = {
+  id: "2kl4V4HU",
+  title: "Latest Sports",
+  type: {
+    value: "feed",
+  },
+  entry: [
+    {
+      id: "X2GbizUD",
+      type: {
+        value: "video",
+      },
+      link: {
+        rel: "self",
+        href: "https://zapp-5409-forum-wday.web.app/jw/media/X2GbizUD?disablePlayNext=false&withErrors=false",
+      },
+      title: "FALL CAMP TOUR: Davies Eagles",
+      summary: "",
+      content: {
+        type: "video/hls",
+        src: "https://cdn.jwplayer.com/manifests/X2GbizUD.m3u8?exp=1691615959&sig=eef3edd0392f860bf552818945226667",
+      },
+      media_group: [
+        {
+          type: "image",
+          media_item: [
+            {
+              key: "320",
+              src: "https://cdn.jwplayer.com/v2/media/X2GbizUD/poster.jpg?width=320",
+            },
+            {
+              key: "480",
+              src: "https://cdn.jwplayer.com/v2/media/X2GbizUD/poster.jpg?width=480",
+            },
+            {
+              key: "640",
+              src: "https://cdn.jwplayer.com/v2/media/X2GbizUD/poster.jpg?width=640",
+            },
+            {
+              key: "720",
+              src: "https://cdn.jwplayer.com/v2/media/X2GbizUD/poster.jpg?width=720",
+            },
+            {
+              key: "1280",
+              src: "https://cdn.jwplayer.com/v2/media/X2GbizUD/poster.jpg?width=1280",
+            },
+            {
+              key: "1920",
+              src: "https://cdn.jwplayer.com/v2/media/X2GbizUD/poster.jpg?width=1920",
+            },
+            {
+              key: "720",
+              src: "https://cdn.jwplayer.com/v2/media/X2GbizUD/poster.jpg?width=720",
+            },
+          ],
+        },
+      ],
+      extensions: {
+        play_next_feed_url:
+          "https://zapp-5409-forum-wday.web.app/jw/playlists/2kl4V4HU?mediaId=WRc743VC",
+        requires_authentication: "true",
+        pubdate: 1691466190,
+        description: "",
+        variations: {},
+        feedId: "2kl4V4HU",
+        duration: 139,
+        formattedRuntime: "02:19",
+      },
+    },
+  ],
+};
